f6ad0d908f43cb4ee646383633508089a93ace5d
Aman katiyar <amankatiyar783597@gmail.com>
2025-01-27 02:47:47 +0530

    cuurncy exchange ended

--- currencyEx/src/comp/InputBox.jsx
+++ currencyEx/src/comp/InputBox.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 
-function InputBox() {
+function InputBox({
+    label,
+    options,
+    fun,
+    cc,
+    Amount,
+    onAmountChange,
+    dis,
+}) {
+
+
   return (
     <>
-        <h1>hello</h1>
+        <div className='flex bg-white p-3 rounded-md flex-col text-black'>
+            <div className='flex justify-between text-gray-600 text-sm w-100 font-semibold '>
+                <div>
+                    {label}
+                </div>
+                <div>
+                    currency Change
+                </div>
+            </div>
+            <div className=' flex justify-between'>
+                <input value={Amount}
+                onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                type='number'
+                disabled ={dis}
+                className=' border-dashed'  ></input>
+                <select value={cc} className=' text-sm' onChange={(e)=>fun(e.target.value)}>
+                    {options.map( ( curr )=>{
+                        return <option key={curr} value={curr} > {curr}</option>
+                    } )}
+                </select>
+
+            </div>
+        </div>
     </>
   )
 }
