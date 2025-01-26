21f648116d64130372e5d9b75afe9b0497745d3d
Aman katiyar <amankatiyar783597@gmail.com>
2025-01-26 12:43:49 +0530

    removed css

--- currencyEx/src/App.jsx
+++ currencyEx/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { InputBox } from './comp'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className=' flex h-screen justify-center items-center text-2xl text-white'>
+        <InputBox/>
+      </div>
     </>
   )
 }
---
+++ currencyEx/src/comp/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputBox() {
+  return (
+    <>
+        <h1>hello</h1>
+    </>
+  )
+}
+
+export default InputBox;
