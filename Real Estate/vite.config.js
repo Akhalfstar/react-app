da2d09f9be9809365403baa9b71f755946db61ed
Aman katiyar <amankatiyar783597@gmail.com>
2025-02-09 12:33:54 +0530

    adede

--- Real Estate/vite.config.js
+++ Real Estate/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    allowedHosts: "q78898-5173.csb.app",
+  },
   plugins: [react() ,  tailwindcss(),],
 })
